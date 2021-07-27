86643056d4dbd83a89828d3e05bee25566431160
Andrés Castro Mamani <castroandresrm@gmail.com>
2021-07-27 08:59:28 -0400

    Agregamos los parametros de la API mapbox

--- models/busquedas.js
+++ models/busquedas.js
@@ -4,11 +4,22 @@ class Busquedas {
     constructor() {
         //leer DB si existe
     }
+    get paramsMapBox() {
+        return {
+            access_token: 'pk.eyJ1IjoiYW5kcmVzY2FzdHJvbWFtYW5pIiwiYSI6ImNrcm0xa2ppcDF1aDYyb280MHkxNTZ3N3MifQ.aJNu81HJ9WA6iSNReTgDnQ',
+            limit: 5,
+            language: 'es'
+        }
+    }
     async ciudad(ciudad = '') {
         try {
             //peticion HTTP
-            const resp = await axios.get('https://reqres.in/api/users?page=2')
-            console.log(resp.data)
+            const instance = axios.create({
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${ ciudad }.json`,
+                params: this.paramsMapBox
+            })
+            const response = await instance.get()
+            console.log(response.data)
 
             return [];
         } catch (error) {
